275c0f827095b44986cd6f913e5c5c725e99851b
Enrique Pereda <epereda@yahoo.com>
2015-06-17 09:43:37 +0100

    Primera pregunta

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// K:importar quiz_controller.js
+var quizController = require('../controllers/quiz_controller');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 // Kike por el curso
 //res.render('index', { title: 'Express' });
   res.render('index', { title: 'Quiz' });
 });
+
+// K: Definición de rutas de /quizes
+router.get('/quizes/question', 				quizController.question);
+router.get('/quizes/answer', 				quizController.answer);
 
 module.exports = router;
