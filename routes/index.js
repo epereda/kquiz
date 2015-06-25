c4846001c730ba329bc94d9ee736f6487903606d
Enrique Pereda <epereda@yahoo.com>
2015-06-25 14:31:36 +0100

    Pagina del Autor

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,17 @@ var quizController = require('../controllers/quiz_controller');
 router.get('/', function(req, res, next) {
 // Kike por el curso
 //res.render('index', { title: 'Express' });
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'K-Quiz' });
 });
 
 // K: Definición de rutas de /quizes
 router.get('/quizes/question', 				quizController.question);
 router.get('/quizes/answer', 				quizController.answer);
+
+// K: Definición de rutas de /author
+router.get('/author', 				function(req, res, next) {
+// Kike para el curso ejercicio
+  res.render('author', { title: 'K-Quiz' });
+});
 
 module.exports = router;
