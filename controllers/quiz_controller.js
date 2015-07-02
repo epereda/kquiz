734f62b7efd288714699ababb1ca9010147dd517
Enrique Pereda <epereda@yahoo.com>
2015-07-02 11:21:27 +0100

    Despliege DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,18 @@
+var models = require('../models/models.js');
+
 // GET /quizes/question
 exports.question = function(req, res) {
-  var resultado = 'Incorrecto';
-    res.render('quizes/question', {pregunta: 'Capital de Italia'});
+  models.Quiz.findAll().then( function(quiz) {
+	res.render('quizes/question', {pregunta: quiz[0].pregunta});
+  })
 };
 // GET /quizes/answer
 exports.answer = function(req, res) {
-  var resultado = 'Incorrecto';
-  if (req.query.respuesta === 'Roma') {
-    res.render('quizes/answer', {respuesta: 'Correcto'});
-  } else {
-    res.render('quizes/answer', {respuesta: 'Incorrecto'});
-  }
+  models.Quiz.findAll().then( function(quiz) {
+	  if (req.query.respuesta === quiz[0].respuesta) {
+	    res.render('quizes/answer', {respuesta: 'Correcto'});
+	  } else {
+	    res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	  }
+  })
 };
